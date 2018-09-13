2d8ffdf785cc0654e59242ff84b61000658a1258
13619849594 <691734536@qq.com>
2018-09-13 09:31:53 +0800

    spring 整合hibernate

---
+++ src/main/java/com/dao/StudentDao.java
@@ -0,0 +1,30 @@
+package com.dao;
+
+import com.entity.StudentEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.stereotype.Repository;
+
+/*
+*@ClassName:StudentDao
+ @Description:TODO
+ @Author:
+ @Date:2018/9/12 17:58 
+ @Version:v1.0
+*/
+@Repository
+public class StudentDao {
+    @Autowired
+    HibernateTemplate hibernateTemplate;
+
+    public static void main(String[] args) {
+        StudentDao studentDao = new StudentDao();
+        studentDao.get(1);
+    }
+
+
+    public StudentEntity get(int id){
+        StudentEntity studentEntity = hibernateTemplate.get(StudentEntity.class, id);
+        return studentEntity;
+    }
+}
